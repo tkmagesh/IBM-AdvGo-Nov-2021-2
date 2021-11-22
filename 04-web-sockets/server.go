7fdac229652b7d97ff53dfcaa437c315b25e9309
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-22 17:05:58 +0530

    chat app using web sockets

--- 04-web-sockets/server.go
+++ 04-web-sockets/server.go
@@ -8,6 +8,8 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var wsConnections []*websocket.Conn
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
@@ -32,8 +34,45 @@ func streamTime(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func chat(w http.ResponseWriter, r *http.Request) {
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	wsConnections = append(wsConnections, ws)
+	err = ws.WriteMessage(websocket.TextMessage, []byte("Welcome to the chat!"))
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	go reader(ws)
+}
+
+func reader(conn *websocket.Conn) {
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		broadcast(string(message))
+	}
+}
+
+func broadcast(message string) {
+	for _, conn := range wsConnections {
+		err := conn.WriteMessage(websocket.TextMessage, []byte(message))
+		if err != nil {
+			log.Println(err)
+			return
+		}
+	}
+}
+
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/time", streamTime)
+	http.HandleFunc("/chat", chat)
 	log.Println(http.ListenAndServe(":8080", nil))
 }
