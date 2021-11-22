c8a65dc76058729598eb592eaabed4957fc11cd0
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-22 12:08:09 +0530

    Create assignment.go

---
+++ 03-http-services/02-httprouter/assignment.go
@@ -0,0 +1,24 @@
+/*
+Expose the REST endpoints for Products
+
+base url :  http://localhost:8080/products
+
+product = { id, name, description, cost }
+
+Possible operations : GET, PUT, PATCH, DELETE, POST
+//Now
+GET - http://localhost:8080/products => return all the products
+GET - http://localhost:8080/products/1 => return the product with id = 1 OR 404 if not found
+POST - http://localhost:8080/products => Save the given product and return with 201 status
+
+//Later
+PUT - http://localhost:8080/products/1 => Update the product with id = 1 OR 404 if not found
+DELETE - http://localhost:8080/products/1 => Delete the product with id = 1 OR 404 if not found
+*/
+
+/*
+json.Marshal(product) & json.Unmarshal(product)
+
+json.NewEncoder(w).Encode(product) => to json
+json.NewDecoder(r.Body).Decode(product) => from json
+*/
