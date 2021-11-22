fd910031611154ed77d2d82a397e0c7e2667f69c
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-22 09:53:25 +0530

    worker - assignment

---
+++ 02-concurrency-patterns/03-worker/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//Any object that has a "Task()" method
+
+var names = []string{
+	"steve",
+	"bob",
+	"mary",
+	"therese",
+	"jason",
+	"Magesh",
+	"Ganesh",
+	"Ramesh",
+	"Rajesh",
+	"Suresh",
+}
+
+type NamePrinter struct {
+	name string
+}
+
+func (np *NamePrinter) Task() {
+	fmt.Println("Name Printer - Name : ", np.name)
+	time.Sleep(2 * time.Second)
+}
+
+func main() {
+	p := worker.New(5)
+	for idx := 0; idx < 2; idx++ {
+		for _, name := range names {
+			np := &NamePrinter{name: name}
+			p.Run(np)
+		}
+	}
+	fmt.Println("All tasks are assigned")
+	p.Shutdown()
+}
