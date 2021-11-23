65ed04712dd2eeafba409a9c7b5c4497d202d05d
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-23 12:52:38 +0530

    server streaming

--- 05-grpc-app/client/client.go
+++ 05-grpc-app/client/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"log"
 
 	"grpc-app/proto"
@@ -19,6 +20,12 @@ func main() {
 	//proxy instance
 	client := proto.NewAppServiceClient(clientConn)
 	ctx := context.Background()
+
+	//doRequestResponse(ctx, client)
+	doServerStreaming(ctx, client)
+}
+
+func doRequestResponse(ctx context.Context, client proto.AppServiceClient) {
 	addRequest := &proto.AddRequest{
 		X: 100,
 		Y: 200,
@@ -31,4 +38,27 @@ func main() {
 		return
 	}
 	fmt.Println(response.GetSum())
+}
+
+func doServerStreaming(ctx context.Context, client proto.AppServiceClient) {
+	primeRequest := &proto.PrimeRequest{
+		Start: 5,
+		End:   100,
+	}
+
+	clientStream, err := client.GeneratePrimes(ctx, primeRequest)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for {
+		response, err := clientStream.Recv()
+		if err == io.EOF {
+			fmt.Println("Thats all folks!")
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+		fmt.Println(response.GetPrime())
+	}
 }
--- 05-grpc-app/server/server.go
+++ 05-grpc-app/server/server.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"grpc-app/proto"
 	"log"
 	"net"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -23,6 +25,34 @@ func (s *server) Add(ctx context.Context, req *proto.AddRequest) (*proto.AddResp
 		Sum: result,
 	}
 	return resp, nil
+}
+
+func (s *server) GeneratePrimes(req *proto.PrimeRequest, stream proto.AppService_GeneratePrimesServer) error {
+	start := req.GetStart()
+	end := req.GetEnd()
+	for i := start; i <= end; i++ {
+		if isPrime(i) {
+			time.Sleep(500 * time.Millisecond)
+			fmt.Printf("Sending Prime : %d\n", i)
+			resp := &proto.PrimeResponse{
+				Prime: i,
+			}
+			if err := stream.Send(resp); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func isPrime(no int32) bool {
+	var i int32
+	for i = 2; i <= no/2; i++ {
+		if no%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
 func main() {
